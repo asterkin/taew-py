550d4028f78cb87ef6d463bf3d4361571f1ca7df
Asher Sterkin <asher.sterkin@gmail.com>
2025-10-23 12:37:05 +0300

    Resolve #9 (#10)

--- taew/adapters/python/ram/for_storing_data/for_configuring_adapters.py
+++ taew/adapters/python/ram/for_storing_data/for_configuring_adapters.py
@@ -21,3 +21,14 @@ class Configure(ConfigureBase):
 
     _ports: str = field(kw_only=True, default="ports")
     _root_marker: str = field(kw_only=True, default="/adapters")
+    _values: dict[str, object] = field(
+        kw_only=True, default_factory=lambda: dict[str, object]()
+    )
+
+    def _collect_kwargs(self) -> dict[str, object]:
+        """Collects keyword arguments for configuring the adapter.
+
+        Returns:
+            dict[str, object]: A dictionary of keyword arguments.
+        """
+        return self._values
